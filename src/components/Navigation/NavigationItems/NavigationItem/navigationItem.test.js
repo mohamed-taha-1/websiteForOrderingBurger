2d18c66b3bb2696be512473577057eb9c88123ff
m6738 <56928006+m6738@users.noreply.github.com>
2020-08-27 05:37:59 +0200

    i deploy the project on local machine using firebase hosting

---
+++ src/components/Navigation/NavigationItems/NavigationItem/navigationItem.test.js
@@ -0,0 +1,18 @@
+
+import React from 'react';
+
+
+import Adapter from 'enzyme-adapter-react-16';
+import {configure , shallow} from 'enzyme';
+
+import NavigationItems from '../NavigationItems';
+import NavigationItem  from './NavigationItem';
+
+configure({adapter: new Adapter()});
+
+descripe('<NavigationItems />',()=>{
+    it('should render two <NavigationItem /> elements if not authenticated',()=>{
+        const wrapper=shallow(<NavigationItems />);
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+});
